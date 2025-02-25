3e473c2974efd6441d358aa3704ec21e28ec0206
MintenkoV <mintenko.vova@gmail.com>
2025-02-25 17:50:12 +0200

    Stable version

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,11 @@
-const path = require("path");
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const path = require('path');
 
 module.exports = (config) => {
   config.resolve.alias = {
     ...config.resolve.alias,
     ...{
-      "@": path.resolve(__dirname, "src/"),
+      '@': path.resolve(__dirname, 'src/'),
     },
   };
   return config;
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React, {JSX} from 'react';
 import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
-import Router from '@/router';
-import { store } from '@/redux/store';
+import Router from './router';
+import { store } from './redux/store';
 
 const App = (): JSX.Element => {
 
